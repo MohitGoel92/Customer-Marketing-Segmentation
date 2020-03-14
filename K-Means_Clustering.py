d1434f694a5ed54832e1054f98f155258490406d
Mohit Goel <55006916+MohitGoel92@users.noreply.github.com>
2020-03-14 23:29:24 +0000

    Add files via upload

---
+++ K-Means_Clustering.py
@@ -0,0 +1,55 @@
+# K Means Clustering
+
+# Importing the libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Importing the dataset
+
+ds = pd.read_csv('Mall_Customers.csv')
+X = ds.iloc[:,[3,4]].values
+
+# Visualising the dataset
+
+plt.scatter(X[:,0], X[:,1], c='blue', s=150)
+plt.title('Customer Dataset')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.show()
+
+# Using the elbow method to find the optimal number of clusters
+
+from sklearn.cluster import KMeans
+
+wcss = []
+
+for i in range(1,16):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', n_init = 10, max_iter = 300, random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1,16), wcss, c='blue')
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Fitting the K-Means to the dataset using 5 clusters
+
+kmeans = KMeans(n_clusters = 5, n_init = 10, max_iter = 300, init = 'k-means++', random_state = 0)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualising the clustered dataset
+
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Careful')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Standard')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Target')
+plt.scatter(X[y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Careless')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Sensible')
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], s=300, c='orange', label = 'Centroid')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend(loc = 'upper left', bbox_to_anchor = (1,1.03))
+plt.show()
